3aaf70ebde25a0b96c5fdfba261f3f122ec00597
5laughter <5laughter.luna@gmail.com>
2019-04-18 08:25:16 +0900

    Create PostList
    이전에 했던 것은 PostList가 아니라 ListPage

--- frontend/src/components/list/PostList/PostList.js
+++ frontend/src/components/list/PostList/PostList.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import styles from './PostList.css';
+import styles from './PostList.scss';
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
-const ConstItem = () => {
+const PostItem = () => {
     return (
         <div className={cx('post-item')}>
             <h2><a>타이틀</a></h2>
+            <div className={cx('date')}>2019-04-18</div>
+            <p>내용</p>
+            <div className={cx('tags')}>
+                <a>#태그</a>
+                <a>#태그</a>
+                <a>#태그</a>
+            </div>
         </div>
     )
 }
+const PostList = () => (
+    <div className={cx('post-list')}>
+        <PostItem/>
+        <PostItem/>
+        <PostItem/>
+        <PostItem/>
+    </div>
+);
+
+export default PostList;
--- frontend/src/pages/ListPage.js
+++ frontend/src/pages/ListPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PageTemplate from 'components/common/PageTemplate';
 import ListWrapper from 'components/list/ListWrapper';
+import PostList from 'components/list/PostList';
 
 const ListPage = () => {
     return (
         <PageTemplate>
             <ListWrapper>
-                냠냠냐먀냐먀냐냐냐먀냐먀냐먀냐냐먀ㅑㄴ먀냐먀냐먀냐냔ㅁ
+                <PostList/>
             </ListWrapper>
         </PageTemplate>
     );
