80ad7304d8eda8ee84e0a998291933690a21bb4d
5laughter <5laughter.luna@gmail.com>
2019-04-25 11:50:15 +0900

    Create Buttons(update, delete) on Header

--- frontend/src/components/common/Header/Header.js
+++ frontend/src/components/common/Header/Header.js
@@ -6,14 +6,20 @@ import Button from 'components/common/Button';
 
 const cx = classNames.bind(styles);
 
-const Header = () => (
+const Header = ({postId, onRemove}) => (
     <header className={cx('header')}>
         <div className={cx('header-content')}>
             <div className={cx('brand')}>
                 <Link to="/">COSMOS</Link>
             </div>
             <div className={cx('right')}>
-                <Button theme="outline" to="/editor">새로운 별</Button>
+                {
+                    postId && [
+                        <Button key="edit" theme="outline" to={`/editor?id=${postId}`}>수정</Button>,
+                        <Button key="remove" theme="outline" onClick={onRemove}>삭제</Button>
+                    ]
+                }
+                <Button theme="outline" to="/editor">새 별</Button>
             </div>
         </div>
     </header>
